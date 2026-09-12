752ee508da99c14518a92fed220b1e23e66e72b7
yes1nt <94116083+yes1nt@users.noreply.github.com>
2026-09-12 14:37:55 +0800

    Create Main.lua

---
+++ API/Deadrails/Main.lua
@@ -0,0 +1,3 @@
+local MainAPI = loadstring(game:HttpGet("https://raw.githubusercontent.com/yes1nt/friedpotato/refs/heads/main/API/MAIN%20API.lua"))()
+local Main = MainAPI:LoadDeadrailsAPI()
+Main:Loader()
